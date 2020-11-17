4bb478556d648da35bc883ab4889b96d9b13ddbc
dilip rao <raodilip54@gmail.com>
2020-11-17 13:14:37 -0800

    commit

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,6 +1,7 @@
-from .views import article_list
+from .views import article_list, article_detail
 from django.urls import path
 
 urlpatterns = [
    path('article/', article_list),
+   path('detail/<int:pk>', article_detail),
 ]
